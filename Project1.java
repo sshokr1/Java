f37e5f307d7b0ac28d46ce69837e7b5523d7bf6d
sshokr1 <73960582+sshokr1@users.noreply.github.com>
2020-11-18 19:29:36 +0200

    Add files via upload
    Task 1 (a)

---
+++ Project1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class FirstProgram {
+	
+     public static void main(String[] args) {
+    	 Scanner traineeName = new Scanner(System.in);
+    	 System.out.println("Please enter your name");
+    	String userinput= traineeName.next();
+		System.out.println("Welcome " + userinput+ " to Java");
+	}   
+
+        
+    
+}
+
